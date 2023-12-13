413ae69b9107affc5ec118f9d9c2c24422ad7683
owechada <owechada@gmail.com>
2023-12-13 11:17:26 +0100

    first commit

---
+++ client/src/api/axios.config.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const baseURL =  "http://localhost:8080/api";
+
+const API = axios.create({
+  baseURL,
+  withCredentials: true,
+});
+
+API.interceptors.request.use(
+  function (req) {
+    const token = JSON.parse(localStorage.getItem("token"));
+    if (token) req.headers["auth-token"] = token;
+    return req;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export default API;
